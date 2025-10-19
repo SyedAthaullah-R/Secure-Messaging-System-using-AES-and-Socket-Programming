fe33aa47da9c9155ad3964db58aa89c02be68d8d
Syed Athuallah R <syedathaullah786786@gmail.com>
2025-10-19 18:23:07 +0530

    Add files via upload

---
+++ Java_AES_With_Socket_Programming/Two_Way_Programming/twoWayServer.java
@@ -0,0 +1,58 @@
+package Two_Way_Programming;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.crypto.SecretKey;
+
+import JAVA_AES.*;
+public class twoWayServer {
+    public static void main(String[] args) throws Exception{
+        AES a = new AES();
+        SecretKey secKey = a.generate_Secret_Key();
+        try{
+            ServerSocket serverSocket = new ServerSocket(8000);
+            System.out.println("Server started successfully, Waiting for the Client...");
+            Socket socket = serverSocket.accept();
+            System.out.println("Client Connected Successfully");
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+            String encodedKey = a.keyToString(secKey);
+            pw.println(encodedKey);
+            System.out.println("Shared AES key sent to the Client Successfully");
+            String clientMsg, serverMsg;
+            while(true){
+                clientMsg = br.readLine();
+                clientMsg = a.decrypt(clientMsg, secKey);
+                if(clientMsg.equalsIgnoreCase("exit")){
+                    System.out.println("The Client disconnected from server");
+                    break;
+                }
+                System.out.println("Client Message: " + clientMsg);
+                System.out.print("Server (Type your message): ");
+                serverMsg = br1.readLine();
+                if(serverMsg.equalsIgnoreCase("exit")){
+                    pw.println(a.encode("exit", secKey));
+                    System.out.println("The Server disconnected");
+                    break;
+                }
+                else{
+                    serverMsg = a.encode(serverMsg, secKey);
+                    pw.println(serverMsg);
+                }
+                
+            }
+            serverSocket.close();
+            socket.close();
+            pw.close();
+            br.close();
+            br1.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
